bab3c02210e8fc0dfcbe282ac3ea544287b7dcb8
cvsmith <cvsmith@andrew.cmu.edu>
2016-08-10 18:49:36 -0400

    Add grounded_sw, remove right_sw

--- quadcopter.ino
+++ quadcopter.ino
@@ -1,30 +1,36 @@
 /**This is an example of using the RCLib with a mega and pinchangeint
  * This example isprovided as is by Jantje
- **/
+ * DON'T USE THE RIGHT SW
+ */
 
-#define NUM_RC_CHANNELS 6 //You need to specify how many pins you want to use
+#define NUM_RC_CHANNELS 5 //You need to specify how many pins you want to use
 #include "PinChangeInt.h"  //If you need pinchangeint you need to include this header
-const uint8_t RC_Channel_Pin[NUM_RC_CHANNELS]={
-  A8,A9,A10,A11,A12,A13};//Here I specify I want to listen to pins A8 to A13 of my mega
+const uint8_t RC_Channel_Pin[NUM_RC_CHANNELS]={A8,A9,A10,A11,A12};
 
-uint16_t RC_Channel_Value[NUM_RC_CHANNELS]; //This variable will contain the values read from the RC signal
+uint16_t RC_Channel_Value[NUM_RC_CHANNELS];
 
 // RC controller variables
 #define ROLL_CHAN 0
 #define YAW_CHAN 1
-#define PITCH_CHAN 3
 #define COLLECTIVE_CHAN 2
+#define PITCH_CHAN 3
+#define GROUNDED_SW_CHAN 4
 
 float yaw_stick, pitch_stick, roll_stick, collective_stick;
+bool grounded_sw;
 
 #define YAW_STICK_OFFSET 1600.0
 #define PITCH_STICK_OFFSET 1550.0
 #define ROLL_STICK_OFFSET 1450.0
-#define COLLECTIVE_STICK_OFFSET 1000.0 // Collective goes from 0 - 1
+#define COLLECTIVE_STICK_OFFSET 1000.0
 #define YAW_STICK_SCALE 446.0
 #define PITCH_STICK_SCALE 485.0
 #define ROLL_STICK_SCALE 485.0
 #define COLLECTIVE_STICK_SCALE 1000.0
+#define GROUNDED_SW_THRESHOLD 1500
+
+// IMU variables
+float p, q, r, phi, theta, psi;
 
 #include "RCLib.h" //This include needs all declarations above. Do not try to move it up or it won't compile
 
@@ -50,14 +56,15 @@ void plot(float Data1, float Data2, float Data3, float Data4=0, float Data5=0, f
 }
 
 /*
- * Populate stick floats with scaled, offset RC signals
+ * Populate rc floats and bools with scaled, offset RC signals
  */
-void get_stick_vals()
+void get_rc_vals()
 {
   yaw_stick = (RC_Channel_Value[YAW_CHAN] - YAW_STICK_OFFSET) / YAW_STICK_SCALE;
   pitch_stick = (RC_Channel_Value[PITCH_CHAN] - PITCH_STICK_OFFSET) / PITCH_STICK_SCALE;
   roll_stick = (RC_Channel_Value[ROLL_CHAN] - ROLL_STICK_OFFSET) / ROLL_STICK_SCALE;
   collective_stick = (RC_Channel_Value[COLLECTIVE_CHAN] - COLLECTIVE_STICK_OFFSET) / COLLECTIVE_STICK_SCALE;
+  grounded_sw = RC_Channel_Value[GROUNDED_SW_CHAN] > GROUNDED_SW_THRESHOLD ? true : false;
 }
 
 void setup()
@@ -72,16 +79,17 @@ void setup()
 void loop()
 {
   //Add your repeated code here
-  get_stick_vals();
+  get_rc_vals();
+  //get_imu_vals();
+  //calc_motor_cmds();
+  //send_motor_cmds();
   
   int flag;
-  if(flag=getChannelsReceiveInfo()) //Here you read the RC flag contains all the channels that have a response
-                                    // see duane's excellent articles on how this works
+  if(flag=getChannelsReceiveInfo()) // see duane's excellent articles on how this works
   {
-    plot(yaw_stick, pitch_stick, roll_stick, collective_stick);
-    //plot(RC_Channel_Value[YAW_CHAN], RC_Channel_Value[PITCH_CHAN], RC_Channel_Value[ROLL_CHAN]);
+    plot(yaw_stick, pitch_stick, roll_stick, collective_stick, grounded_sw);
   }
-  delay(50); //don't flood your serial monitor
+  //delay(50);
 
 }
 
